3839332e3a6745983ad9866e8cd13d3f84f358c0
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-10 14:23:36 +0800

    My branch

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Add cryptographic key #
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Hardest-Password-in-world'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
