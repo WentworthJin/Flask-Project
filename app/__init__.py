99cfd2705d6c949f7ce4f5e72f64a62ec40da27d
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-10 14:46:43 +0800

    Delete old mig

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config # From congig.py import the Config class #
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config) # Tell Flask to read it and apply it #
+db = SQLAlchemy(app) # use db Represent the database #
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
