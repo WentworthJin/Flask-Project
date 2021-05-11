ebbfa8b0da3ceb74680ccfa48bd21ac098b45d7c
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-11 19:59:49 +0800

    Change login

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from config import Config # From congig.py import the Config class #
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .admin import admin
-from flask_admin.contrib.sqla import ModelView
-from .models import User, Post
+from flask_login import LoginManager
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_object(Config) # Tell Flask to read it and apply it #
@@ -14,5 +13,3 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
-admin.add_view(ModelView(User,db.session))
-admin.add_view(ModelView(Post,db.session))
--- app/admin.py
+++
@@ -1,7 +0,0 @@
-from flask_admin import Admin
-import app
-from flask import Blueprint, render_template
-
-admin=Admin(app, name="Admin System")
-
-
