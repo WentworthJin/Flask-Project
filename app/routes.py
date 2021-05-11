889c34864ac2d07551adca3940b7afa277c8ece7
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-11 18:27:39 +0800

    Fix Login

--- app/routes.py
+++ app/routes.py
@@ -56,7 +56,7 @@ def test():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return homepage()
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -64,24 +64,21 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
-        return redirect(next_page)
+        return homepage()
     return render_template('login.html', title='Sign In', form=form)
     
 @app.route('/logout')
 def logout():
     logout_user()
     flash("You have logout")
-    return redirect(url_for('home'))
+    return homepage()
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         flash('You already login!')
-        return redirect(url_for('home'))
+        return homepage()
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
