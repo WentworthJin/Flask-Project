2ccaed09b68c5a087d8152186fa808d62cf1f8ce
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-11 20:51:47 +0800

    Add init admin

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config # From congig.py import the Config class #
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_object(Config) # Tell Flask to read it and apply it #
@@ -10,5 +11,7 @@ db = SQLAlchemy(app) # use db Represent the database #
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 from app import routes, models
