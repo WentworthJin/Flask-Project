73fa1298bf468fbeeb4ed013ddb40fe10c5cf3b7
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-11 20:11:31 +0800

    Edit init

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask
 from config import Config # From congig.py import the Config class #
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .admin import admin
-from flask_admin.contrib.sqla import ModelView
-from .models import User, Post
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -15,5 +12,3 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
-admin.add_view(ModelView(User,db.session))
-admin.add_view(ModelView(Post,db.session))
