6a4424770bbc45686681000fca16519931490c1b
Steven Z <71420444+KebingZ@users.noreply.github.com>
2021-05-11 18:40:34 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 from config import Config # From congig.py import the Config class #
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from .admin import admin
+from flask_admin.contrib.sqla import ModelView
+from .models import User, Post
 
 app = Flask(__name__)
 app.config.from_object(Config) # Tell Flask to read it and apply it #
@@ -12,3 +14,5 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
+admin.add_view(ModelView(User,db.session))
+admin.add_view(ModelView(Post,db.session))
