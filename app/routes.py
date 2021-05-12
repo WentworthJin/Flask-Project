10fb2186e55cce2b5cffba0e4fe3b508bb7cc3ea
Liangbo Jin <23078811@student.uwa.edu.au>
2021-05-12 10:03:23 +0800

    Edit flash

--- app/routes.py
+++ app/routes.py
@@ -50,27 +50,31 @@ def inhert():
 @app.route('/Test')
 @login_required
 def test():
-    return render_template('Testbase.html')
+    if current_user.is_authenticated:
+        flash('Let\' start the mini test, ')
+        return render_template('Testbase.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        flash('You have successfully login!')
         return homepage()
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash('Invalid username or password!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        flash('You have successfully login!')
         return homepage()
     return render_template('login.html', title='Sign In', form=form)
     
 @app.route('/logout')
 def logout():
     logout_user()
-    flash("You have logout")
+    flash("You have been logout!", "info")
     return homepage()
 
 
