9fe314160e5e7fc350314112f3598bce272c0579
KebingZ <71420444+KebingZ@users.noreply.github.com>
2021-05-16 16:32:21 +0800

    ...

--- testBot.py
+++ testBot.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver,common
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest, time, re
 from app.models import User
@@ -9,56 +9,61 @@ class myTest(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.base_url = "http://127.0.0.1:5000/"
-        app.config["SQLALCHEMY_DATABASE_URI"]="sqlite://"
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
         db.create_all()
 
-
     def testCase(self):
-        u=["Arran", "Bob", "Sam", "Tim", "Tom", "Hacker", "Lam", "Zikkel"]
-        p=["123456", "000000", "122222", "121212", "242424242", "353553535", "993993", "jijiefe"]
+        u = ["Arran", "Bob", "Sam", "Tim", "Tom", "Hacker", "Lam", "Zikkel"]
+        p = ["123456", "000000", "122222", "121212", "242424242", "353553535", "993993", "jijiefe"]
         return u, p
 
     def test01(self):
-        self.driver.maximize_window()
-        self.driver.get(self.base_url)
-        time.sleep(2)
-        self.driver.find_element_by_class_name("loginbutton").click()
-        time.sleep(1)
-        self.driver.find_element_by_class_name("register_button").click()
-        time.sleep(1)
-        name = "feijigeigege"
-        email = 'egiegiengie@gmail.com'
-        password = "1234567"
-        self.driver.find_element_by_name('username').send_keys(name)
-        time.sleep(0.5)
-        self.driver.find_element_by_name('email').send_keys(email)
-        time.sleep(0.5)
-        self.driver.find_element_by_name('password').send_keys(password)
-        time.sleep(0.5)
-        self.driver.find_element_by_name('password2').send_keys(password)
-        time.sleep(0.5)
-        self.driver.find_element_by_name('submit').click()
-        time.sleep(1)
-        text1 = self.driver.find_element_by_class_name('redp').text
-        print(text1, "\nTest1 finished!")
+        try:
+            self.driver.maximize_window()
+            self.driver.get(self.base_url)
+            time.sleep(2)
+            self.driver.find_element_by_class_name("loginbutton").click()
+            time.sleep(1)
+            self.driver.find_element_by_class_name("register_button").click()
+            time.sleep(1)
+            name = "feijigeigege"
+            email = 'egiegiengie@gmail.com'
+            password = "1234567"
+            self.driver.find_element_by_name('username').send_keys(name)
+            time.sleep(0.5)
+            self.driver.find_element_by_name('email').send_keys(email)
+            time.sleep(0.5)
+            self.driver.find_element_by_name('password').send_keys(password)
+            time.sleep(0.5)
+            self.driver.find_element_by_name('password2').send_keys(password)
+            time.sleep(0.5)
+            self.driver.find_element_by_name('submit').click()
+            time.sleep(1)
+            text1 = self.driver.find_element_by_class_name('redp').text
+            print(text1, "\nTest1 finished!")
+        except common.exceptions.NoSuchElementException as e:
+            print(e, "\nDue to non-running server.")
 
     def test02(self):
-        self.driver.maximize_window()
-        self.driver.get(self.base_url)
-        time.sleep(1)
-        self.driver.find_element_by_xpath("//ul[@class = 'ul_mainstyle']//li[2]").click()
-        time.sleep(1)
-        items = self.driver.find_elements_by_xpath("//ul[@class = 'sideul']//li[not(@class)]")
-        for _ in items[1:len(items)-1]:
-            self.driver.find_element_by_class_name("next").click()
-            time.sleep(0.8)
-        text2 = self.driver.find_element_by_class_name("congrats").text
-        print(text2, "\nTest2 finished!")
+        try:
+            self.driver.maximize_window()
+            self.driver.get(self.base_url)
+            time.sleep(1)
+            self.driver.find_element_by_xpath("//ul[@class = 'ul_mainstyle']//li[2]").click()
+            time.sleep(1)
+            items = self.driver.find_elements_by_xpath("//ul[@class = 'sideul']//li[not(@class)]")
+            for _ in items[1:len(items) - 1]:
+                self.driver.find_element_by_class_name("next").click()
+                time.sleep(0.8)
+            text2 = self.driver.find_element_by_class_name("congrats").text
+            print(text2, "\nTest2 finished!")
+        except common.exceptions.NoSuchElementException as e:
+            print(e,"\nDue to non-running server.")
 
     def testUser(self):
 
         for i in range(len(self.testCase()[0])):
-            user=User(username=self.testCase()[0][i])
+            user = User(username=self.testCase()[0][i])
             user.set_password(self.testCase()[1][i])
             self.assertFalse(user.check_password("111111"))
             self.assertTrue(user.check_password(self.testCase()[1][i]))
@@ -69,6 +74,6 @@ class myTest(unittest.TestCase):
         db.session.remove()
         db.drop_all()
         self.driver.close()
-        
-    if __name__ == "__main__":
-        unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
